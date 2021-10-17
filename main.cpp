be73a3e6f80393745d5d72c1b35ad83f33feb374
MatiasMarro <matiasmarro1987@gmail.com>
2021-10-17 17:32:04 -0300

    Gravity simulation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+void pintar_nave(float cx, float cy,BITMAP *buffer);
+void mover_nave(float &cx, float &cy,float &vx, float &vy);
+
+
 int main()
 {
     allegro_init();
@@ -11,34 +15,61 @@ int main()
     set_gfx_mode(GFX_AUTODETECT_WINDOWED, 740 ,500, 0, 0);
     BITMAP *buffer = create_bitmap(740, 500);
 
-    int x1,x2,y1,y2;
+    float cx,cy;
+    float vx,vy;
+    cx=100;cy=100;
 
-    x1=0;y1=0;x2=30;y2=30;
 
     while(!key[KEY_ESC]){
         clear_to_color(buffer, 0x000000);
-        rectfill(buffer, x1,y1,x2,y2, 0xeaeae6);
+        pintar_nave(cx,cy,buffer);
         blit(buffer,screen,0,0,0,0,740,500);
-        if(key[KEY_RIGHT]){
-            x1 +=10;
-            x2 +=10;
-        }
-        if(key[KEY_LEFT]){
-            x1 -=10;
-            x2 -=10;
-        }
-        if(key[KEY_UP]){
-            y1 -=10;
-            y2 -=10;
-        }
-        if(key[KEY_DOWN]){
-            y1 +=10;
-            y2 +=10;
-        }
-        rest(1);
+        mover_nave(cx,cy,vx,vy);
+        rest(20);
+
+
     }
 
 
     return 0;
 }
 END_OF_MAIN();
+
+
+
+void pintar_nave(float cx, float cy,BITMAP *buffer){
+   /* line(buffer, cx-20, cy+20,cx-20,cy+10, 0xeaeae6);
+    line(buffer, cx-20, cy+20,cx-20,cy+10, 0xeaeae6);
+    line(buffer, cx-20, cy+10,cx-10,cy, 0xeaeae6);
+    line(buffer, cx-10, cy,cx-10,cy-10, 0xeaeae6);
+    line(buffer, cx-10, cy-10,cx,cy-15, 0xeaeae6);
+    line(buffer, cx, cy-15,cx+10,cy-10, 0xeaeae6);
+    line(buffer, cx+10, cy-10,cx+10,cy, 0xeaeae6);
+    line(buffer, cx+10, cy,cx+20,cy+10, 0xeaeae6);
+    line(buffer, cx+20, cy+10,cx+20,cy+20, 0xeaeae6);
+    line(buffer, cx-10, cy,cx+10,cy, 0xeaeae6);
+                    UNA FORMA DE PINTAR LA NAVE */
+    float nav[26] =
+    {cx-20,cy+20,    cx-20,cy+10,     cx-10,cy,   cx-10,cy-10,    cx,cy-15,      cx+10,cy-10,
+     cx+10,cy,       cx+20,cy+10,       cx+20,cy+20,       cx-10,cy,    cx+10,cy};
+
+     for(int i=0; i<=14;i+=2){
+        line(buffer,nav[i],nav[i+1],nav[i+2],nav[i+3],0x999999);
+        if(i == 14) line (buffer,nav[i+4],nav[i+5],nav[i+6],nav[i+7],0x999999);
+     }
+
+}
+
+void mover_nave(float &cx, float &cy,float &vx, float &vy){
+    float ax,ay;
+    ax=0;
+    ay = 0.05;
+
+    vx += ax;
+    vy += ay;
+
+    cx +=vx;
+    cy +=vy;
+
+
+}
