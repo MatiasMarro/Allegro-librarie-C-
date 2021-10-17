fd080c66fa59b819e9837e761e9e4895f68db023
MatiasMarro <matiasmarro1987@gmail.com>
2021-10-17 18:19:20 -0300

    Setting motor acceleration and fire print

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include<allegro.h>
+#include<cmath>
 
 using namespace std;
 
 void pintar_nave(float cx, float cy,BITMAP *buffer);
 void mover_nave(float &cx, float &cy,float &vx, float &vy);
+void rotar(float &x, float &y, float cx ,float cy , float da);
+void aceleracion(float da, float &vx, float &vy);
+void pintar_motor(float da, float cx, float cy, BITMAP *buffer);
 
 
 int main()
@@ -22,9 +26,25 @@ int main()
 
     while(!key[KEY_ESC]){
         clear_to_color(buffer, 0x000000);
+        mover_nave(cx,cy,vx,vy);
+
+        if(key[KEY_UP]){
+            aceleracion(0,vx,vy); // Cuando el angulo = 0, vamos a tener una aceleracion hacia arriba
+            pintar_motor(0,cx,cy,buffer);
+        }
+        if(key[KEY_RIGHT]){
+            aceleracion(-90,vx,vy);
+            pintar_motor(-90,cx,cy,buffer);
+        }
+        if(key[KEY_LEFT]){
+            aceleracion(90,vx,vy);
+            pintar_motor(90,cx,cy,buffer);
+        }
+
         pintar_nave(cx,cy,buffer);
         blit(buffer,screen,0,0,0,0,740,500);
-        mover_nave(cx,cy,vx,vy);
+
+
         rest(20);
 
 
@@ -62,8 +82,8 @@ void pintar_nave(float cx, float cy,BITMAP *buffer){
 
 void mover_nave(float &cx, float &cy,float &vx, float &vy){
     float ax,ay;
-    ax=0;
-    ay = 0.05;
+    ax = 0;
+    ay = 0.1;
 
     vx += ax;
     vy += ay;
@@ -71,5 +91,41 @@ void mover_nave(float &cx, float &cy,float &vx, float &vy){
     cx +=vx;
     cy +=vy;
 
+
+}
+
+void rotar(float &x, float &y, float cx ,float cy , float da){
+    float dx = x - cx;
+    float dy = y - cy;
+    float r = sqrt(dx*dx + dy*dy);
+    float a = atan2(dy,dx);
+    float da_rad = da / 180 * M_PI;
+
+    a -= da_rad;
+
+    x = cx + r*cos(a);
+    y = cy + r*sin(a);
+}
+
+void aceleracion(float da, float &vx, float &vy){
+    float ax = 0;
+    float ay = -0.15;
+    rotar(ax,ay,0,0,da); // Si da = 0 el vector aceleracion no se modifica
+    vx += ax;
+    vy += ay;
+}
+
+void pintar_motor(float da, float cx, float cy, BITMAP *buffer){
+    float c1,c2;
+    c1 = cx; c2 =cy;
+    if(da != 0) c2 += 9;
+    float fuego[14] = {c1-5,c2+5   ,c1-10,c2+20   ,c1-5,c2+20   ,c1,c2+35   ,c1+5,c2+20   ,c1+10,c2+20   ,c1+5,c2+5 };
+
+    for(int i =0;i<=12 ; i+=2){
+        rotar(fuego[i],fuego[i+1],cx,cy,da);
+    }
+    for(int i =0;i<=10 ; i+=2){
+        line(buffer, fuego[i],fuego[i+1],fuego[i+2],fuego[i+3],0x999999);
+    }
 
 }
