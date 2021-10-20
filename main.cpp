ebc3178d5dd14f5128a0187bf5aeab0ee1d7bdd3
MatiasMarro <matiasmarro1987@gmail.com>
2021-10-19 23:40:36 -0300

    Version 1.0 end

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ void pintar_motor(float da, float cx, float cy, BITMAP *buffer);
 void medidor_combustible(bool gastar_combustible,float &combustible,BITMAP *buffer);
 
 void pintar_nivel(int num_nivel, BITMAP *buffer );
+void Explocion(float cx, float cy, BITMAP *buffer,int num_nivel);
+bool Game_Over(float &cx, float &cy,float &vx,float &vy,int &num_nivel,float &combustible, BITMAP *buffer);
+bool aterrizar(float cx, float cy, float vx, float vy, BITMAP *buffer, int num_nivel);
+bool choque_triangulo(float x1, float y1, float x2, float y2, float p1x, float p1y, float p2x, float p2y, string tipo_triangulo);
+bool choque_nave(int num_nivel, float cx, float cy);
+void avanzar_nivel(float &cx,float &cy, float &vx, float &vy, int &num_nivel, float &combustible,BITMAP *buffer);
+void reiniciar_nivel(float &cx,float &cy,float &vx,float &vy, float &combustible);
 
 
 int main()
@@ -24,12 +31,12 @@ int main()
 
     float cx,cy;
     float vx,vy;
-    cx=100;cy=100;
+    cx=710;cy=100; vy=-2;
     float combustible = 100; // 100 significa tanque lleno
-    int num_nivel = 2;
+    int num_nivel = 3;
 
 
-    while(!key[KEY_ESC]){
+    while(!key[KEY_ESC] && !Game_Over(cx,cy,vx,vy,num_nivel,combustible,buffer)){
         bool gastar_combustible = false;
         clear_to_color(buffer, 0x000000);
         pintar_nivel(num_nivel,buffer);
@@ -51,12 +58,15 @@ int main()
             gastar_combustible = true;
         }
 
+
+
         medidor_combustible(gastar_combustible,combustible,buffer);
         pintar_nave(cx,cy,buffer);
         blit(buffer,screen,0,0,0,0,740,500);
+        avanzar_nivel(cx,cy,vx,vy,num_nivel,combustible,buffer);
 
 
-        rest(10);
+        rest(15);
 
 
     }
@@ -94,7 +104,7 @@ void pintar_nave(float cx, float cy,BITMAP *buffer){
 void mover_nave(float &cx, float &cy,float &vx, float &vy){
     float ax,ay;
     ax = 0;
-    ay = 0.095;
+    ay = 0.098;
 
     vx += ax;
     vy += ay;
@@ -145,20 +155,223 @@ void medidor_combustible(bool gastar_combustible,float &combustible,BITMAP *buff
     textout_centre_ex(buffer,font,"combostible",100,30, 0x999999, 0x00000);
     rectfill(buffer,50,50,50+combustible,55,0x999999);
     if(gastar_combustible == true){
-        combustible -=0.2;
+        combustible -=0.1;
     }
 }
 
 void pintar_nivel(int num_nivel, BITMAP *buffer ){
     if(num_nivel == 1){
-        rectfill(buffer, 10,450,100,500,0x999999);
+        rectfill(buffer, 10,450,100,500,0xEFEF00);
     }
     if(num_nivel == 2){
-        triangle(buffer,400,500,300,500,300,200,0x999999);
-        triangle(buffer,400,0,500,0,500,400,0x999999);
-        triangle(buffer,620,500,700,500,620,300,0x999999);
-       rectfill(buffer, 10,450,100,500,0x999999);
+        triangle(buffer, 110, 100, 300, 500, 110 , 500 ,0x999999);
+        triangle(buffer, 500, 500, 600, 300, 600 , 500 ,0x999999);
+        triangle(buffer, 600, 300, 800, 600, 600 , 500 ,0x999999);
+        triangle(buffer, 200,   0, 400, 350, 400 ,   0 ,0x999999);
+        rectfill(buffer, 10, 450, 100, 500,      0xEFEF00);
+    }
+    if(num_nivel == 3){
+        triangle(buffer, 110, 300, 300, 500, 110 , 500 ,0x999999);
+        triangle(buffer, 305, 300, 510, 500, 310 , 500 ,0x999999);
+        triangle(buffer, 400,   0, 600, 400, 600 ,   0 ,0x999999);
+        triangle(buffer, 100,   0, 250, 200, 250 ,   0 ,0x999999);
+        rectfill(buffer, 10, 450, 100, 500,      0xEFEF00);
+
     }
 
 
 }
+
+void Explocion(float cx, float cy, BITMAP *buffer,int num_nivel){
+    float x[12]={cx-10, cx+10, cx   , cx   , cx+15, cx-15, cx+5, cx-10, cx+10, cx-5,cx-10,cx+10};
+    float y[12]={cy   , cy   , cy-15,cy+15 ,cy-15 ,cy+15 , cy+5, cy-10, cy-10,cy+10,cy   ,cy};
+
+    float dx[6]={7,     7,      0,      -7,     -7,     0};
+    float dy[6]={0,     -7,     -7,     -7,     0,      7};
+
+
+
+    clear(screen);
+    do{
+        clear(buffer);
+        pintar_nivel(num_nivel,buffer);
+        int j=0;
+        for(int i=0;i<=10;i+=2){
+            line(buffer , x[i]  , y[i], x[i+1], y[i+1], 0xFFFFFF);
+            rotar(x[i+1],y[i+1] , x[i], y[i]  ,  15);
+
+            x[i]+=dx[j];
+            y[i]+=dy[j];
+            x[i+1]+=dx[j];
+            y[i+1]+=dy[j];
+            j++;
+        }
+
+        textout_centre_ex(buffer,font,"PRESS (A) FOR TRY AGAIN",370,240,0xFBFF00,0x000000);
+        textout_centre_ex(buffer,font,"PRESS (ESC) TO EXIT",370,250,0xFBFF00,0x000000);
+        blit(buffer,screen,0,0,0,0,740,500);
+        rest(25);
+
+    }while(!key[KEY_ESC] && !key[KEY_A]);
+}
+
+
+bool Game_Over(float &cx, float &cy,float &vx,float &vy,int &num_nivel,float &combustible, BITMAP *buffer){
+    if(cx+20 >= 740 || cx-20 <= 0 || cy-20 <=0 || cy+15 <=0 || cy+20 >= 500){
+
+        Explocion(cx,cy,buffer,num_nivel);
+        reiniciar_nivel(cx,cy,vx,vy,combustible);
+        //return true;
+    }
+   else if(choque_nave(num_nivel,cx,cy) == true){
+        Explocion(cx,cy,buffer,num_nivel);
+        reiniciar_nivel(cx,cy,vx,vy,combustible);
+        //return true;
+    }
+
+    else if(cy+20 >=450 && cx-20 >=10 && cx+20 <=100 && vy > 1.5){
+        Explocion(cx,cy,buffer,num_nivel);
+        reiniciar_nivel(cx,cy,vx,vy,combustible);
+    }
+    else if(cx-20 <= 100 && 100<=cx+20 && cy+20 >=450 ){
+            Explocion(cx,cy,buffer,num_nivel);
+        reiniciar_nivel(cx,cy,vx,vy,combustible);
+    }
+    return false;
+}
+
+
+bool aterrizar(float cx, float cy, float vx, float vy, BITMAP *buffer, int num_nivel){
+    if(cy+20 >=450 && cx-20 >=10 && cx+20 <=100 ){
+        if(vy<=1.5) {
+                return  true;
+        }else {
+
+        }
+    }
+    return false;
+}
+
+
+bool choque_triangulo(float x1, float y1, float x2, float y2, float p1x, float p1y, float p2x, float p2y, string tipo_triangulo ){
+    float m= (y2-y1)/(x2-x1); // Si m>0 triangulo derecho, si m<0 trianglo espejado
+    float b = y1-m*x1; // Ecuacion de la recta
+if(tipo_triangulo == "abajo"){
+    if(m >0 ){
+        if(x1 <= p1x && p1x <= x2){
+            if(p2y >= m*p1x + b ) return true;
+            }
+            else{
+                if(p1x <=x1 && x1 <= p2x)
+                    if( y1 <=p2y) return true;
+            }
+    }
+    else if(m <0 ){
+        if(x1 <= p2x && p2x <= x2){
+            if(p2y >= m*p2x + b ) return true;
+            }
+            else{
+                if(p1x <=x2 && x2 <= p2x)
+                    if( y2 <= p2y) return true;
+            }
+    }
+    }
+
+    if(tipo_triangulo == "arriba"){
+        if(m>0){
+        if(x1 <= p2x && p2x <= x2){
+                if(p1y <= m*p2x+b) return true;
+            }
+            else{
+                if(p1x <= x2 && x2 <= p2x)
+                if(p1y <=y2) return true;
+            }
+            return false;
+    }
+    else{
+        if(x1 <=p1x && p1x <= x2){
+            if(p1y <= m*p1x+b) return true;
+        }
+        else{
+            if(p1x <=x1 && x1<= p2x)
+            if(p1y <=y1) return true;
+        }
+        return false;
+        }
+    }
+    return false;
+}
+
+
+
+
+bool choque_nave(int num_nivel, float cx, float cy){
+
+    //pata izquierda de la nave
+    float r1x =cx-20 , r1y= cy ;
+    float r2x=cx-10  , r2y= cy+20 ;
+    //pata derecha de la nave
+    float p1x =cx+10 , p1y= cy ;
+    float p2x=cx+20  , p2y= cy+20 ;
+    //cuerpo de la nave
+    float q1x =cx-10 , q1y= cy-15 ;
+    float q2x=cx+10  , q2y= cy ;
+
+    if(num_nivel == 2){
+        if(choque_triangulo(110,100,300,500,r1x,r1y,r2x,r2y,"abajo") == true || choque_triangulo(110,100,300,500,p1x,p1y,p2x,p2y,"abajo")== true || choque_triangulo(110,100,300,500,q1x,q1y,q2x,q2y,"abajo")== true) return true;
+        else if(choque_triangulo(500,500,600,300,r1x,r1y,r2x,r2y,"abajo") == true || choque_triangulo(500,500,600,300,p1x,p1y,p2x,p2y,"abajo")== true || choque_triangulo(500,500,600,300,q1x,q1y,q2x,q2y,"abajo")== true) return true;
+        else if(choque_triangulo(600,300,800,500,r1x,r1y,r2x,r2y,"abajo") == true || choque_triangulo(600,300,800,500,p1x,p1y,p2x,p2y,"abajo")== true || choque_triangulo(600,300,800,300,q1x,q1y,q2x,q2y,"abajo")== true) return true;
+        else if(choque_triangulo(200,0  ,400,350,r1x,r1y,r2x,r2y,"arriba") == true || choque_triangulo(200,0  ,400,350,p1x,p1y,p2x,p2y,"arriba")== true || choque_triangulo(200,0  ,400,350,q1x,q1y,q2x,q2y,"arriba")== true) return true;
+
+    }
+    else if(num_nivel == 3){
+        if(choque_triangulo(110,100,300,500,r1x,r1y,r2x,r2y,"abajo")  || choque_triangulo(110,100,300,500,p1x,p1y,p2x,p2y,"abajo") || choque_triangulo(110,100,300,500,q1x,q1y,q2x,q2y,"abajo")) return true;
+        else if(choque_triangulo(305, 500, 510, 500,r1x,r1y,r2x,r2y,"abajo") == true || choque_triangulo(305, 500, 510, 500,p1x,p1y,p2x,p2y,"abajo")== true || choque_triangulo(305, 500, 510, 500,q1x,q1y,q2x,q2y,"abajo")== true) return true;
+        else if(choque_triangulo(400,0,600,400,r1x,r1y,r2x,r2y,"arriba") == true || choque_triangulo(400,0,600,400,p1x,p1y,p2x,p2y,"arriba")== true || choque_triangulo(400,0,600,400,q1x,q1y,q2x,q2y,"arriba")== true) return true;
+        else if(choque_triangulo(100,0  ,250,200,r1x,r1y,r2x,r2y,"arriba") == true || choque_triangulo(100,0  ,250,200,p1x,p1y,p2x,p2y,"arriba")== true || choque_triangulo(100,0  ,250,200,q1x,q1y,q2x,q2y,"arriba")== true) return true;
+
+    }
+    return false;
+
+}
+
+
+
+
+void avanzar_nivel(float &cx,float &cy, float &vx, float &vy, int &num_nivel, float &combustible,BITMAP *buffer){
+    if(aterrizar(cx,cy,vx,vy,buffer,num_nivel)==true){
+        cx = 710;
+        cy =100; vy =-1 ;vx=0;
+        if(num_nivel !=3) num_nivel++;
+
+        while(!key[KEY_A]){
+            textout_centre_ex(screen,font,"PRESS (A) FOR THE NEXT LEVEL",370,250,0xFBFF00,0x000000);
+            rest(20);
+        }
+        combustible = 100;
+    }
+}
+
+
+
+
+
+void reiniciar_nivel(float &cx,float &cy,float &vx,float &vy,float &combustible){
+    cx= 710;
+    cy=100;
+    vx=0;vy=-1;
+    combustible=100;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
